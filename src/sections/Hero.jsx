eb0603b6baefb7b9410a0e133a6d86a186038d2d
Vencent Delute <iceiceicebell@gmail.com>
2025-11-14 17:30:47 +0800

    adjust hero z index

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
 
   return (
     <section className="min-h-screen w-full flex flex-col relative" id="home">
-      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3 z-10">
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
           Hi, I am Vencent <span className="waving-hand">👋</span>
         </p>
